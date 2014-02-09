33e8353a76a0f46d3eaa2af0395e31859e0c1d5c
Philip John <phil@philipjohn.co.uk>
2014-02-09 18:32:45 +0000

    Updating plugin to use new Widgets API

--- theyworkforyou.php
+++ theyworkforyou.php
@@ -26,178 +26,193 @@ Future features list;
     http://wtfpl.net
 */
 
-// The settings page for OL
-function twfy_settings(){
-	
-	// The form has been submitted, so do the dirty work
-	if ($_POST['twfy_hidden'] == "Y"){
-		// Log the ID number of the MP
-		$twfy_person_id = trim(htmlentities($_POST['twfy_person_id']));
-		$twfy_title = trim($_POST['twfy_title']);
-		$twfy_desc = trim(htmlentities($_POST['twfy_desc']));
-		$twfy_date = trim(htmlentities($_POST['twfy_date']));
-		$twfy_limit = trim(htmlentities($_POST['twfy_limit']));
-		$twfy_link = trim(htmlentities($_POST['twfy_link']));
-        $twfy_options = array(
-            'person_id' => $twfy_person_id,
-            'title' => $twfy_title,
-            'desc' => $twfy_desc,
-            'date' => $twfy_date,
-            'limit' => $twfy_limit,
-            'link' => $twfy_link
-        );
-		update_option('twfy_recent_activity_widget', $twfy_options);
-        
-		echo '<div class="updated"><p><strong>'. __('Options saved.' ) .'</strong></p></div>';
+/**
+ * Adds MPs_Recent_Activity widget.
+ */
+class MPs_Recent_Activity extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+				'mps_recent_activity', // Base ID
+				__( 'MPs Recent Activity' ), // Name
+				array( 'description' => __( 'A widget showing the latest activity from a Member of Parliament' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+		
+		if ( ! empty( $instance['mp'] ) ) {
+			$xml = simplexml_load_file("http://www.theyworkforyou.com/api/getHansard?key=AMznwDBcpK3gCLwTTMC9PYHJ&output=xml&person=".$instance['mp']); // Load XML
+			
+			echo "<ul>\n";
+			$i = 0; //counter for number of meetings
+			foreach ( $xml->rows->match as $match ) {
+				
+				if ( $i >= $instance['limit'] ) // don't list more than X meetings
+					break;
+				
+				$date = (string) $match->hdate;
+				$date = strtotime($date);
+				
+				echo '<li>';
+				
+				if ( $instance['date'] == 1 ) {
+					echo date( 'j M', $date ) . ': ';
+				}
+				
+				$url = wp_kses( (string) $match->listurl );
+				$url = esc_url( 'http://www.theyworkforyou.com' . $url );
+				$body = wp_kses( (string) $match->parent->body );
+				echo "<a href=\"$url\">$body</a>";
+				
+				if ( $instance['description'] == 1 ) {
+					$description = wp_kses( (string) $match->extract );
+					echo '<br/>' . $description;
+				}
+				
+				echo '</li>' . "\n";
+				
+				$i++; //increment the counter
+			}
+			echo "</ul>\n";
+			
+			if ( $instance['link'] ) {
+				// Link back to the MPs page on TWFY
+				$MPurl = esc_url( (string) $xml->rows->match->speaker->url );
+				echo '<p>More from <a href="http://www.theyworkforyou.com' . $MPurl . '">TheyWorkForYou.com</a></p>';
+			}
+		}
+		
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		
+		$defaults = array(
+			'title' => __( 'MPs Recent Activity' ),
+			'mp' => 10777,
+			'description' => true,
+			'date' => true,
+			'limit' => 5,
+			'link' => true,
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		
+		# Checkbox states
+		$description = $instance['description'] ? 'checked="checked"' : '';
+		$date = $instance['date'] ? 'checked="checked"' : '';
+		$link = $instance['link'] ? 'checked="checked"' : '';
+		
+		$mps = $this->get_mps();
+		
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>">
+		</p>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id( 'mp' ); ?>"><?php _e( 'Choose an MP:' ); ?></label> 
+			<select id="<?php echo $this->get_field_id( 'mp' ); ?>" name="<?php echo $this->get_field_name( 'mp' ); ?>">
+				<?php
+					foreach ( $mps as $mid => $mname ) {
+						echo '<option value="' . $mid . '"'
+							. selected( $instance['mp'], $mid, false )
+							. '>'. $mname . '</option>';
+					}
+				?>
+			</select>
+		</p>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id( 'limit' ); ?>"><?php _e( 'How many items should be shown?' ); ?></label> 
+			<input id="<?php echo $this->get_field_id( 'limit' ); ?>" name="<?php echo $this->get_field_name( 'limit' ); ?>" type="text" value="<?php echo absint( $instance['limit'] ); ?>" size="3">
+		</p>
+		
+		<p>
+			<input class="checkbox" type="checkbox" <?php echo $description; ?> id="<?php echo $this->get_field_id( 'description' ); ?>" name="<?php echo $this->get_field_name( 'description' ); ?>">
+			<label for="<?php echo $this->get_field_id( 'description' ); ?>"><?php _e( 'Show description?' ); ?></label>
+			<br/>
+			
+			<input class="checkbox" type="checkbox" <?php echo $date; ?> id="<?php echo $this->get_field_id( 'date' ); ?>" name="<?php echo $this->get_field_name( 'date' ); ?>">
+			<label for="<?php echo $this->get_field_id( 'date' ); ?>"><?php _e( 'Show date?' ); ?></label>
+			<br/>
+			
+			<input class="checkbox" type="checkbox" <?php echo $link; ?> id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>">
+			<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Add a link to this MP on TheyWorkForYou?' ); ?></label>
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['mp'] = absint( $new_instance['mp'] );
+		$instance['limit'] = absint( $new_instance['limit'] );
+		$instance['description'] = $new_instance['description'] ? 1 : 0;
+		$instance['date'] = $new_instance['date'] ? 1 : 0;
+		$instance['link'] = $new_instance['link'] ? 1 : 0;
+
+		return $instance;
 	}
 	
-	// Load the councils XML for choosing the right authority
-	$xml = simplexml_load_file('http://theyworkforyou.com/api/getMPs?key=AMznwDBcpK3gCLwTTMC9PYHJ&output=xml');
-	
-	// Retrieve the currently selected council, if there is one.
-	$twfy_options = get_option('twfy_recent_activity_widget');
-    
-    // re-organise MPs into an array for sorting on name
-    $MPs = array();
-    foreach ($xml->match as $MP){
-        $MPid = (string )$MP->person_id;
-        $MPname = (string )$MP->name;
-        $MPs[$MPid] = $MPname;
-    }
-    asort($MPs); // actually sort it.
-	
-	?>
-	<div class="wrap">
-		<h2><?php _e('TheyWorkForYou Settings'); ?></h2>
-		<form name="twfy_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
-			<input type="hidden" name="twfy_hidden" value="Y">  
-			<h3><?php _e('Choose your MP'); ?></h3>
-			<p>
-				<select name="twfy_person_id" id="twfy_person_id">
-					<?php
-						foreach ($MPs as $MP_id => $MP_name){
-							echo '<option value="'.$MP_id.'"';
-							if ($twfy_options['person_id'] == $MP_id){
-								echo ' selected="selected"';
-							}
-							echo '>'.$MP_name.'</option>'."\n";
-						}
-					?>
-				</select>
-			</p>
-			
-            <fieldset>
-                <h3>Recent Activity Widget - Options</h3>
-                <p>
-                    <label for="twfy_title">Widget title: </label>
-                    <input type="text" id="twfy_title" name="twfy_title" value="<?php echo stripslashes($twfy_options['title']);?>" />
-                </p>
-                <p>
-                    Show description?<br/>
-                    <label for="twfy_desc_yes"><input type="radio" id="twfy_desc_yes" name="twfy_desc" value="1" <?php if ($twfy_options['desc']==1){echo 'checked="checked" ';} ?>/> Yes</label><br/>
-                    <label for="twfy_desc_no"><input type="radio" id="twfy_desc_no" name="twfy_desc" value="0" <?php if ($twfy_options['desc']==0){echo 'checked="checked" ';} ?>/> No</label>
-                </p>
-                <p>
-                    Show date?<br/>
-                    <label for="twfy_date_yes"><input type="radio" id="twfy_date_yes" name="twfy_date" value="1" <?php if ($twfy_options['date']==1){echo 'checked="checked" ';} ?>/> Yes</label><br/>
-                    <label for="twfy_date_no"><input type="radio" id="twfy_date_no" name="twfy_date" value="0" <?php if ($twfy_options['date']==0){echo 'checked="checked" ';} ?>/> No</label>
-                </p>
-                <p>
-                    <label for="twfy_limit">How many items should be shown?: </label>
-                    <input type="text" id="twfy_limit" name="twfy_limit" value="<?php echo $twfy_options['limit'];?>" />
-                </p>
-                <p>
-                    Show link to MP on TheyWorkForYou.com?<br/>
-                    <label for="twfy_link_yes"><input type="radio" id="twfy_link_yes" name="twfy_link" value="1" <?php if ($twfy_options['link']==1){echo 'checked="checked" ';} ?>/> Yes</label><br/>
-                    <label for="twfy_link_no"><input type="radio" id="twfy_link_no" name="twfy_link" value="0" <?php if ($twfy_options['link']==0){echo 'checked="checked" ';} ?>/> No</label>
-                </p>
-            </fieldset>
-			<p class="submit">  
-				<input type="submit" name="Submit" value="<?php _e('Update Options') ?>" />
-			</p>  
-		</form>
-	</div>
-	<?php
+	function get_mps() {
+		# @todo Add setting for API key
+		$xml = simplexml_load_file('http://theyworkforyou.com/api/getMPs?key=AMznwDBcpK3gCLwTTMC9PYHJ&output=xml');
+		
+		$mps = array(); # for storage
+		
+		foreach ( $xml->match as $m ){
+			$pid = (int) $m->person_id;
+			$pname = (string) $m->name;
+			$mps[$pid] = $pname;
+		}
+		
+		asort($mps);
+		
+		return $mps;
+	}
+
+} // class MPs_Recent_Activity
+
+// register MPs_Recent_Activity widget
+function register_mps_recent_activity_widget() {
+	register_widget( 'MPs_Recent_Activity' );
 }
+add_action( 'widgets_init', 'register_mps_recent_activity_widget' );
 
-// Add the settings page
-function twfy_actions(){
-	add_options_page('TheyWorkForYou Settings', 'TheyWorkForYou', 5, 'TheyWorkForYou', 'twfy_settings');
-}
-
-
-// Recent activity widget
-function twfy_recent_activity_widget($args){
-	extract($args); // Prep to display widget code
-    
-    $twfy_options = get_option('twfy_recent_activity_widget');
-	
-	echo $before_widget;
-	echo $before_title.stripslashes($twfy_options['title']).$after_title;
-	twfy_recent_activity_widget_contents();
-	echo $after_widget;
-}
-
-// Recent activity DASHBOARD widget
-function twfy_recent_activity_dbwidget(){
-    twfy_recent_activity_widget_contents();
-}
-
-// Contents for recent activity widgets
-function twfy_recent_activity_widget_contents(){
-	$twfy_options = get_option('twfy_recent_activity_widget'); // The council we're displaying
-    
-    if ($twfy_person_id !== FALSE){ // Not if the ID isn't set.
-        $xml = simplexml_load_file("http://www.theyworkforyou.com/api/getHansard?key=AMznwDBcpK3gCLwTTMC9PYHJ&output=xml&person=".$twfy_options['person_id']); // Load XML
-        
-        echo "<ul>\n";
-        $i = 0; //counter for number of meetings
-        foreach ($xml->rows->match as $match){
-            if ($i>=$twfy_options['limit']) { break; } // don't list more than 5 meetings
-            $date = strtotime($match->hdate);
-            echo '<li>';
-            if ($twfy_options['date']==1){ echo date('j M', $date).': '; }
-            echo '<a href="http://www.theyworkforyou.com'.$match->listurl.'">'.$match->parent->body.'</a>';
-            if ($twfy_options['desc']==1){ echo '<br/>'.$match->body; }
-            echo '</li>'."\n";
-            $i++; //increment the counter
-        }
-        echo "</ul>\n";
-        
-        if ($twfy_options['link']==1){
-            // Link back to the MPs page on TWFY
-            $MPurl = (string )$xml->rows->match->speaker->url;
-            echo '<p>More from <a href="http://www.theyworkforyou.com'.$MPurl.'">TheyWorkForYou.com</a></p>';
-        }
-    }
-    else {
-        echo "<p>Sorry, no MP has been selected. Please select an MP from the settings page.";
-    }
-}
-
-
-
-// Dashboard widgets init function 
-function twfy_add_dashboard_widgets(){
-	wp_add_dashboard_widget('twfy-recent-activity-widget', 'MPs Recent Activity', 'twfy_recent_activity_dbwidget');
-}
-
-// Initialising function
-function twfy_init(){
-	register_sidebar_widget(__('MPs Recent Activity'), 'twfy_recent_activity_widget');
-    $twfy_default_options = array(
-        'person_id'=>'10068',
-        'title'=>'MPs recent activity',
-        'desc'=>1,
-        'date'=>1,
-        'limit'=>5,
-        'link'=>1
-    );
-    add_option('twfy_recent_activity_widget', $twfy_default_options);
-}
-
-add_action("plugins_loaded", "twfy_init");
-add_action('admin_menu', 'twfy_actions');
-add_action('wp_dashboard_setup', 'twfy_add_dashboard_widgets');
 
 ?>
