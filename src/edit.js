078e7c6094d9ba59d65f73c0ccbf40dd936343bf
Philip John <phil@philipjohn.me.uk>
2021-12-08 19:34:36 +0000

    Refactor to a functional component

--- src/edit.js
+++ src/edit.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment } from '@wordpress/element';
+import { Fragment, useState, useEffect } from '@wordpress/element';
 import {
 	Placeholder,
 	SelectControl,
@@ -28,66 +28,32 @@ import { InspectorControls } from '@wordpress/block-editor';
  */
 import './editor.scss';
 
-class Edit extends Component {
+const Edit = ( props ) => {
+	const { attributes, setAttributes, isSelected } = props;
+	const { currentMP, noOfEntries } = attributes;
+	const [ listOfMPs, setListOfMPs ] = useState([]);
+	const [ activity, setActivity ] = useState([]);
 
-	/**
-	 * Constructor.
-	 */
-	constructor() {
-		super( ...arguments );
-		this.state = {
-			listofMPs: [],
-			activity: []
-		}
+	const getListOfMPs = () => {
+		apiFetch( { path: '/twfy/v1/get_mps_names_for_dropdown' } ).then( response =>
+			setListOfMPs( response )
+		);
 	}
 
-	componentDidMount() {
-		const { currentMP, noOfEntries } = this.props.attributes;
-
-		apiFetch( { path: '/twfy/v1/get_mps_names_for_dropdown' } ).then( response =>
-			this.setState( { listofMPs: response } )
-		);
+	const getActivity = () => {
 		apiFetch( { path: '/twfy/v1/get_mp_details_for_activity/' + currentMP + '/' + noOfEntries } )
 			.then( response =>
-				this.setState( { activity: response } )
+				setActivity( response )
 			);
 	}
 
-	componentDidUpdate( prevProps ) {
-		const { currentMP, noOfEntries } = this.props.attributes;
-		const { activity } = this.state;
-
-		// If the user has changed the MP, grab the new MP details and update state.
-		if ( prevProps.attributes.currentMP !== currentMP ) {
-			apiFetch( { path: '/twfy/v1/get_mp_details_for_activity/' + currentMP + '/' + noOfEntries } )
-				.then( response =>
-					this.setState( { activity: response } )
-				);
-		}
-
-		// If the user changes the number of entries, update state accordingly.
-		if ( prevProps.attributes.noOfEntries !== noOfEntries ) {
-
-			// If we're only removing items, just modify the array, rather than query the API.
-			if ( prevProps.attributes.noOfEntries > noOfEntries ) {
-				activity.items = activity.items.splice( 0, noOfEntries );
-				this.setState( { activity: activity } );
-			} else {
-				apiFetch( { path: '/twfy/v1/get_mp_details_for_activity/' + currentMP + '/' + noOfEntries } )
-					.then( response =>
-						this.setState( { activity: response } )
-					);
-			}
-		}
-	}
-
-	MPsForSelect = listofMPs => {
+	const MPsForSelect = listOfMPs => {
 		return [
 			{
 				label: __('Select an MP'),
 				value: null,
 			},
-			...Object.values( listofMPs ).map( mp => {
+			...Object.values( listOfMPs ).map( mp => {
 				return {
 					label: mp.name,
 					value: mp.person_id,
@@ -96,30 +62,33 @@ class Edit extends Component {
 		];
 	}
 
-	render() {
-		const { attributes, setAttributes, isSelected } = this.props;
-		const { listofMPs, activity } = this.state;
-		const { currentMP, noOfEntries } = attributes;
+	useEffect( () => {
+		getListOfMPs()
+	}, [ listOfMPs ] )
 
-		return (
-			<Fragment>
-				{ isSelected && (
-					<Placeholder>
-						{ ! listofMPs && <Spinner /> }
-						{ listofMPs && !! listofMPs.length && (
-							<SelectControl
-								label={ __( 'Select an MP' ) }
-								value={ currentMP }
-								options={ this.MPsForSelect( listofMPs ) }
-								onChange={ _currentMP => setAttributes( { currentMP: parseInt( _currentMP ) } ) }
-								/>
-						) }
-						{ listofMPs && ! listofMPs.length && (
-							__( 'No MPs list found.' )
-						) }
-					</Placeholder>
-				) }
-				
+	useEffect( () => {
+		getActivity()
+	}, [ currentMP, noOfEntries ] )
+
+	return (
+		<Fragment>
+			{ isSelected && (
+				<Placeholder>
+					{ ! listOfMPs && <Spinner /> }
+					{ listOfMPs && !! listOfMPs.length && (
+						<SelectControl
+							label={ __( 'Select an MP' ) }
+							value={ currentMP }
+							options={ MPsForSelect( listOfMPs ) }
+							onChange={ _currentMP => setAttributes( { currentMP: parseInt( _currentMP ) } ) } />
+					) }
+					{ listOfMPs && ! listOfMPs.length && (
+						__( 'No MPs list found.' )
+					) }
+				</Placeholder>
+			) }
+			
+			{ activity && ( 
 				<div className="wp-block-theyworkforyou-mps-recent-activity">
 					<h2>Recent activity by { activity.fullName } MP</h2>
 					<ul className="mps-activity">
@@ -138,22 +107,21 @@ class Edit extends Component {
 						) ) }
 					</ul>
 				</div>
-				
-				<InspectorControls>
-					<PanelBody title={ __('MPs Recent Activity') }>
-						<RangeControl
-							label={ __('Number of items to show') }
-							value={ noOfEntries }
-							onChange={ _noOfEntries => setAttributes( { noOfEntries: _noOfEntries } ) }
-							min={ 1 }
-							max={ 100 }
-							required
-							/>
-					</PanelBody>
-				</InspectorControls>
-			</Fragment>
-		)
-	}
+			) }
+			
+			<InspectorControls>
+				<PanelBody title={ __('MPs Recent Activity') }>
+					<RangeControl
+						label={ __('Number of items to show') }
+						value={ noOfEntries }
+						onChange={ _noOfEntries => setAttributes( { noOfEntries: _noOfEntries } ) }
+						min={ 1 }
+						max={ 100 }
+						required />
+				</PanelBody>
+			</InspectorControls>
+		</Fragment>
+	)
 }
 
 Edit.propTypes = {
